0e67421ab2a785a7228893f7f27b89565652d833
xinnjie <xinnjie31@gmail.com>
2025-10-15 10:49:37 +0800

    fix: onekeymap-cli view return error when ~/.config/onekeymap/onekeymap.json not exist

--- internal/cmd/view.go
+++ internal/cmd/view.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
@@ -10,6 +12,7 @@ import (
 	"github.com/xinnjie/onekeymap-cli/internal/keymap"
 	"github.com/xinnjie/onekeymap-cli/internal/mappings"
 	"github.com/xinnjie/onekeymap-cli/internal/views"
+	keymapv1 "github.com/xinnjie/onekeymap-cli/protogen/keymap/v1"
 )
 
 type viewFlags struct {
@@ -41,7 +44,17 @@ func viewRun(f *viewFlags, dependencies func() *mappings.MappingConfig) func(_ *
 		}
 		file, err := os.Open(path)
 		if err != nil {
-			return fmt.Errorf("failed to open onekeymap config: %w", err)
+			if !errors.Is(err, os.ErrNotExist) {
+				return fmt.Errorf("failed to open onekeymap config: %w", err)
+			}
+			if err := ensureConfigFile(path); err != nil {
+				return err
+			}
+
+			file, err = os.Open(path)
+			if err != nil {
+				return fmt.Errorf("failed to open onekeymap config: %w", err)
+			}
 		}
 		defer func() { _ = file.Close() }()
 
@@ -54,4 +67,27 @@ func viewRun(f *viewFlags, dependencies func() *mappings.MappingConfig) func(_ *
 		_, err = tea.NewProgram(m).Run()
 		return err
 	}
+}
+
+// Create empty config file if it doesn't exist
+func ensureConfigFile(path string) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0o750); err != nil {
+		return fmt.Errorf("failed to create config directory: %w", err)
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o600)
+	if err != nil {
+		return fmt.Errorf("failed to initialize onekeymap config: %w", err)
+	}
+
+	if err := keymap.Save(file, &keymapv1.Keymap{}); err != nil {
+		_ = file.Close()
+		return fmt.Errorf("failed to initialize onekeymap config: %w", err)
+	}
+
+	if err := file.Close(); err != nil {
+		return fmt.Errorf("failed to close onekeymap config: %w", err)
+	}
+
+	return nil
 }
